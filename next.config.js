69afd4f7ccde1a2809942f5a3fa36e01cbe4d2c9
Yousef Aburadi <65052747+yousef2007-rgb@users.noreply.github.com>
2023-01-24 02:41:46 +0300

    ui changes

--- next.config.js
+++ next.config.js
@@ -1,3 +1,17 @@
 module.exports = {
-  reactStrictMode: true,
-}
+  async headers() {
+    const headers = [];
+    if (process.env.NEXT_PUBLIC_VERCEL_ENV === "preview") {
+      headers.push({
+        headers: [
+          {
+            key: "X-Robots-Tag",
+            value: "noindex",
+          },
+        ],
+        source: "/:path*",
+      });
+    }
+    return headers;
+  },
+};
