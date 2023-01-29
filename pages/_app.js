35b5590c60da05fc373e10bfec0a9bd75a5a9009
Yousef Aburadi <65052747+yousef2007-rgb@users.noreply.github.com>
2023-01-29 18:47:21 +0300

    adding analytics

--- pages/_app.js
+++ pages/_app.js
@@ -1,10 +1,12 @@
 import "../styles/globals.scss";
 import { store } from "../store";
 import { Provider } from "react-redux";
+import { Analytics } from "@vercel/analytics/react";
 function MyApp({ Component, pageProps }) {
   return (
     <Provider store={store}>
       <Component {...pageProps} />
+      <Analytics />
     </Provider>
   );
 }
