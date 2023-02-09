1db2689682ffa3f37624d1fbffe7d5008544e3d9
Yousef Aburadi <65052747+yousef2007-rgb@users.noreply.github.com>
2023-02-10 00:09:44 +0300

    add loading to navigation links

--- components/ui/Header/Menu.jsx
+++ components/ui/Header/Menu.jsx
@@ -1,25 +1,32 @@
-import Link from "next/link";
-import React from "react";
+import Link from 'next/link';
+import React from 'react';
+import Loading from '../Loading';
+import { useState } from 'react';
 
 export default function Menu({ data }) {
-  return (
-    <div className=" absolute top-full right-1/2 translate-x-1/2  w-full   mx-auto flex bg-white border-t-4 border-secondery min-w-fit ">
-      {data.map((item, index) => (
-        <section key={index} className=" flex flex-col w-60">
-          <img src={item.image} alt="" />
-          <div className=" flex flex-col">
-            {item.items.map((element, index) => (
-              <Link
-                key={index}
-                className="font-bold p-3 capitalize transition-all hover:bg-secondery w-full hover:text-white"
-                href={element.link}
-              >
-                {element.text}
-              </Link>
-            ))}
-          </div>
-        </section>
-      ))}
-    </div>
-  );
+	const [isLoading, setIsLoading] = useState(false);
+	return (
+		<>
+			<Loading visablilty={isLoading} />
+			<div className=" absolute top-full right-1/2 mx-auto  flex   w-full min-w-fit translate-x-1/2 border-t-4 border-secondery bg-white ">
+				{data.map((item, index) => (
+					<section key={index} className=" flex w-60 flex-col">
+						<img src={item.image} alt="" />
+						<div className=" flex flex-col">
+							{item.items.map((element, index) => (
+								<Link
+									key={index}
+									className="w-full p-3 font-bold capitalize transition-all hover:bg-secondery hover:text-white"
+									href={element.link}
+									onClick={() => setIsLoading(true)}
+								>
+									{element.text}
+								</Link>
+							))}
+						</div>
+					</section>
+				))}
+			</div>
+		</>
+	);
 }
