40516c346b15ff3395a921b4647c71c7547f9953
Yousef Aburadi <65052747+yousef2007-rgb@users.noreply.github.com>
2023-02-17 03:16:14 +0300

    try to fix the headers problem

--- next.config.js
+++ next.config.js
@@ -1,12 +1,12 @@
 module.exports = {
-  async headers() {
-    const headers = [];
-    if (process.env.NEXT_PUBLIC_VERCEL_ENV === "preview") {
-      headers.push({
-        headers: [{}],
-        source: "/:path*",
-      });
-    }
-    return headers;
-  },
+	async headers() {
+		const headers = [];
+		if (process.env.NEXT_PUBLIC_VERCEL_ENV === "preview") {
+			headers.push({
+				headers: [],
+				source: "/:path*",
+			});
+		}
+		return headers;
+	},
 };
