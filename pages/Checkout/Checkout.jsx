fa022a9998af2cca9a83801ecc4fa47f51be65fe
Yousef Aburadi <65052747+yousef2007-rgb@users.noreply.github.com>
2023-02-20 23:15:23 +0300

    Update Checkout.jsx

--- pages/Checkout/Checkout.jsx
+++ pages/Checkout/Checkout.jsx
@@ -27,8 +27,8 @@ export default function Checkout() {
 		<>
 			<Header />
 			{cartItems.length != 0 ? (
-				<main className=" mx-auto mt-20 flex  h-fit max-w-7xl flex-col justify-center p-5">
-					<div className=" mx-auto flex w-full max-w-2xl flex-col justify-evenly">
+				<main className=" mx-auto mt-20 flex  h-fit max-w-2xl flex-col justify-center p-5">
+					<div className=" flex w-full  flex-col justify-evenly">
 						{cartItems.map((item, index) => (
 							<CartItem key={index} {...item} />
 						))}
