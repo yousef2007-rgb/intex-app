bddeeeadcc298791b82eeaa9c578755ba85e65d0
Yousef Aburadi <65052747+yousef2007-rgb@users.noreply.github.com>
2023-04-01 03:00:43 +0300

    add google analytics

--- pages/_app.js
+++ pages/_app.js
@@ -3,9 +3,22 @@ import { store } from "../store";
 import { Provider } from "react-redux";
 import { Analytics } from "@vercel/analytics/react";
 import Loading from '../components/ui/Loading'
+import Script from "next/script";
 function MyApp({ Component, pageProps }) {
 	return (
 		<Provider store={store}>
+			<Script strategy="lazyOnload" src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}`} />
+
+			<Script strategy="lazyOnload">
+				{`
+		window.dataLayer = window.dataLayer || [];
+		function gtag(){dataLayer.push(arguments);}
+		gtag('js', new Date());
+		gtag('config', '${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}', {
+		page_path: window.location.pathname,
+		});
+	`}
+			</Script>
 			<Component {...pageProps} />
 			<Analytics />
 			<Loading />
