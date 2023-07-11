52a5f38640bb5bdd568d815edd197e3733db7da0
Yousef Aburadi <65052747+yousef2007-rgb@users.noreply.github.com>
2023-07-11 19:11:13 +0300

    Update index.jsx

--- components/HomePageComponents/Intro/index.jsx
+++ components/HomePageComponents/Intro/index.jsx
@@ -7,7 +7,7 @@ export default function Intro({ intros }) {
 				<a
 					style={{
 						width: item.aspectRatio == '1/1' ? '23%' : '100%',
-						minWidth: '170px',
+						minWidth: '150px',
 					}}
 					className="m-1 rounded-xl shadow-xl transition-all hover:shadow-2xl"
 					key={index}
