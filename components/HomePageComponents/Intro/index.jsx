e6ec38e66ca92c8f4a759b595823083fdb6b0702
Yousef Aburadi <65052747+yousef2007-rgb@users.noreply.github.com>
2023-07-21 03:30:46 +0300

    some tweaks to the padding in the intro component

--- components/HomePageComponents/Intro/index.jsx
+++ components/HomePageComponents/Intro/index.jsx
@@ -6,9 +6,9 @@ export default function Intro({ intros }) {
 			{intros.map((item, index) => (
 				<a
 					className={`my-1  ${
-						item.aspectRatio == '1/1' ? 'w-[49%]' : 'w-[99%]'
+						item.aspectRatio == '1/1' ? 'w-[47%]' : 'w-[97%]'
 					}  rounded-xl shadow-xl transition-all hover:shadow-2xl ${
-						item.aspectRatio == '1/1' ? 'mobile:w-[24%]' : ''
+						item.aspectRatio == '1/1' ? 'mobile:w-[23%]' : ''
 					} `}
 					key={index}
 					href={item.link}
