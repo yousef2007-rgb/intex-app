41c1917c1ecae064c9088841abbecc4c9f6137af
Yousef Aburadi <65052747+yousef2007-rgb@users.noreply.github.com>
2023-08-13 21:56:08 +0300

    Update index.jsx

--- components/ui/Header/Navigation/index.jsx
+++ components/ui/Header/Navigation/index.jsx
@@ -13,7 +13,7 @@ export default function Navigation({
 
 	return (
 		<nav
-			className={`fixed top-0 left-0 z-40 h-screen w-full max-w-xs flex-col items-center bg-white p-5 transition-all tablet:w-[35vw] tablet:max-w-none`}
+			className={`fixed top-0 left-0 z-40 h-screen  w-full max-w-xs flex-col items-center overflow-auto bg-white p-5 transition-all tablet:w-[35vw] tablet:max-w-none`}
 			style={{
 				transform: `translate(${
 					navigationVisability == 'flex' ? '0' : '-1000px'
