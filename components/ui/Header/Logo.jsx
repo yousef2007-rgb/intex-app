24de6993f77bc76d3ec3ca9463d5fa88a453f71b
Yousef Aburadi <65052747+yousef2007-rgb@users.noreply.github.com>
2023-08-28 21:02:41 +0300

    Update Logo.jsx

--- components/ui/Header/Logo.jsx
+++ components/ui/Header/Logo.jsx
@@ -5,7 +5,7 @@ export default function Logo({ navigation }) {
 	const { lang } = useRouter().query;
 	return (
 		<Link
-			className="mr-auto w-3/4 min-w-[100px] max-w-[150px] tablet:mx-0"
+			className="mr-auto w-3/4 min-w-[100px] max-w-[150px] tablet:!mx-0"
 			href={`/?lang=${lang == 'arabic' ? 'arabic' : 'english'}`}
 			style={{
 				marginLeft: !navigation ? 'auto' : 'unset',
